dd3aed864942baabbabccc99a407c487491c0379
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-06 23:39:03 +0530

    Add : Function to Calculate Length Of Line

--- Line_Comparision_Problem/Program.cs
+++ Line_Comparision_Problem/Program.cs
@@ -6,7 +6,19 @@ namespace Line_Comparision_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To The Line Comparision Problem");
+            
+            Console.WriteLine("Welcome to the Line Comparision Computation Program");
+            Console.WriteLine("Enter your X1,X2,Y1,Y2:");
+            int X1 = Convert.ToInt32(Console.ReadLine());
+            int X2 = Convert.ToInt32(Console.ReadLine());
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your X1 is: " + X1 + "\nYour X2 is: " + X2 + "\nYour Y1 is: " + Y1 + "\nYour Y2 is: " + Y2);
+            double Lenth_of_line = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+            Console.WriteLine("Your Lenth of line is: " + Lenth_of_line);
+      
         }
+
     }
 }
+
