9c3df448f576dc59da0740039452e6b0ae09ee86
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-06 23:51:02 +0530

    Add : function to check two lines are equal or not

--- Line_Comparision_Problem/Program.cs
+++ Line_Comparision_Problem/Program.cs
@@ -1,24 +1,60 @@
 ﻿using System;
 
-namespace Line_Comparision_Problem
+namespace LineComparisionComputationProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        int X1, X2, Y1, Y2;
+        double Lenth_of_Line;
+        public void AcceptLineCoOrdinates()
         {
-            
-            Console.WriteLine("Welcome to the Line Comparision Computation Program");
             Console.WriteLine("Enter your X1,X2,Y1,Y2:");
-            int X1 = Convert.ToInt32(Console.ReadLine());
-            int X2 = Convert.ToInt32(Console.ReadLine());
-            int Y1 = Convert.ToInt32(Console.ReadLine());
-            int Y2 = Convert.ToInt32(Console.ReadLine());
+            X1 = Convert.ToInt32(Console.ReadLine());
+            X2 = Convert.ToInt32(Console.ReadLine());
+            Y1 = Convert.ToInt32(Console.ReadLine());
+            Y2 = Convert.ToInt32(Console.ReadLine());
+        }
+        public void DisplayLineCoOrdinates(int X1, int X2, int Y1, int Y2)
+        {
             Console.WriteLine("Your X1 is: " + X1 + "\nYour X2 is: " + X2 + "\nYour Y1 is: " + Y1 + "\nYour Y2 is: " + Y2);
-            double Lenth_of_line = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
-            Console.WriteLine("Your Lenth of line is: " + Lenth_of_line);
-      
         }
 
+        public double CalCulateLengthofLine(int X1, int X2, int Y1, int Y2)
+        {
+            Lenth_of_Line = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+            return Lenth_of_Line;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparision Computation Program");
+
+            Program lineobject = new Program();
+            Console.WriteLine("Enter The First Line Co-Ordinates :");
+            lineobject.AcceptLineCoOrdinates();
+            Console.WriteLine("The First Line Co-Ordinates are :");
+            lineobject.DisplayLineCoOrdinates(lineobject.X1, lineobject.X2, lineobject.Y1, lineobject.Y2);
+            lineobject.CalCulateLengthofLine(lineobject.X1, lineobject.X2, lineobject.Y1, lineobject.Y2);
+            double Lenth_of_First_Line = lineobject.Lenth_of_Line;
+            Console.WriteLine("The first line length is :" + Lenth_of_First_Line);
+
+            Console.WriteLine("Enter The Second Line Co-Ordinates :");
+            lineobject.AcceptLineCoOrdinates();
+            Console.WriteLine("The Second Line Co-Ordinates are :");
+            lineobject.DisplayLineCoOrdinates(lineobject.X1, lineobject.X2, lineobject.Y1, lineobject.Y2);
+            lineobject.CalCulateLengthofLine(lineobject.X1, lineobject.X2, lineobject.Y1, lineobject.Y2);
+            double Lenth_of_Second_Line = lineobject.Lenth_of_Line;
+            Console.WriteLine("The second line length is :" + Lenth_of_Second_Line);
+
+            if (Lenth_of_First_Line == Lenth_of_Second_Line)
+            {
+                Console.WriteLine("The Two lines Are Equals");
+            }
+            else
+            {
+                Console.WriteLine("The Two lines Are Not Equals");
+            }
+
+        }
     }
 }
-
